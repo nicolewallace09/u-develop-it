4482079bb746e956981dd3e4356284ddfb723af1
nicolewallace09 <nicole.elisaw@gmail.com>
2020-05-27 14:41:34 -0700

    api end points for candidates

--- server.js
+++ server.js
@@ -23,17 +23,68 @@ const db = new sqlite3.Database('./db/election.db', err => {
     console.log('Connected to the election database.');
   });
 
-// GET route
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: 'Hello World'
-//     });
+// GET a single candidate 
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates
+               WHERE id = ?`; 
+  const params = [req.params.id]; 
+  db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message }); 
+        return; 
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+  });
+}); 
+
+// create a candidate 
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//              VALUES (?,?,?,?)`; 
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// ES5 function. not arrow function, to use this
+// db.run(sql, params, function(err, result) {
+//   if (err) {
+//     console.log(err); 
+//   }
+//   console.log(result, this.lastID); 
 // }); 
 
-// all runs the SQL query and executes the callback with all the resulting rows 
-db.all(`SELECT * FROM candidates`, (err, row) => {
-  console.log(rows); 
+// delete a candidate 
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`; 
+  const params = [req.params.id]; 
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return; 
+    }
+    res.json({
+      message: 'successfully deleted',
+      changes: this.changes
+    });
+  });
 }); 
+
+// all runs the SQL query and executes the callback with all the resulting rows 
+// receive all candidates 
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT * FROM candidates`; 
+  const params = []; 
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json ({
+      message: 'success',
+      data: rows
+    });
+  }); 
+});
 
 // default response for any other request (Not Found) Catch all
 app.use((req, res) => {
