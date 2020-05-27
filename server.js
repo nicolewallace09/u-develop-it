dd527648345e2b80f28e1b2638af7d35f11134f3
nicolewallace09 <nicole.elisaw@gmail.com>
2020-05-26 19:32:05 -0700

    adding modules to server

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// import sqlite3 // verbose - to produce messages in the terminal regarding the state of the runtime 
+const sqlite3 = require('sqlite3').verbose();
+
 // import express
 const express = require('express'); 
 
@@ -11,6 +14,15 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json()); 
 
+// connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+      return console.error(err.message);
+    }
+  
+    console.log('Connected to the election database.');
+  });
+
 // GET route
 // app.get('/', (req, res) => {
 //     res.json({
@@ -23,7 +35,10 @@ app.use((req, res) => {
     res.status(404).end();
 }); 
 
-// listen function
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`); 
+// start server after DB connection 
+db.on('open', () => {
+    // listen function
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`); 
+    }); 
 }); 
